a4fbc05d201a9ec872af6b697576db0cb62eeb2d
KoraIrene <irenegwartney@gmail.com>
2022-04-13 22:37:34 +0000

    Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :models
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 Rails.application.routes.draw do
+  devise_for :models
   root "projects#index"
   resources :projects
 end
