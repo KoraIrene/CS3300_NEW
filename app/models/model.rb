5ef7b088cc177441ff63c03b34cdac903c4e2c9d
KoraIrene <irenegwartney@gmail.com>
2022-04-14 03:23:56 +0000

    Jsio

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,6 +1,8 @@
 class Model < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  belongs_to: Users
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
